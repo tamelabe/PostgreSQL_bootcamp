f49d247643e3867962c083db193e783551259252
Tamela Bears <tamelabe@student.21-school.ru>
2023-02-11 21:19:02 +0300

    Release

--- src/ex07/day04_ex07.sql
+++ src/ex07/day04_ex07.sql
@@ -14,12 +14,14 @@ WITH v_id AS (
     JOIN menu m on pizz.id = m.pizzeria_id
     WHERE price < 800),
     v_pizz_id AS (
-    SELECT id
-    FROM pre1_pizz_id v1
-    WHERE (SELECT p2.id FROM pre2_pizz_id p2 WHERE p2.id) NOT IN v1.id LIMIT 1)
+    SELECT v2.id
+    FROM pre2_pizz_id v2
+    WHERE v2.id NOT IN (SELECT id FROM pre1_pizz_id) LIMIT 1)
 INSERT INTO person_visits
 VALUES ((SELECT * FROM v_id),
         (SELECT * FROM v_per_id),
         (SELECT * FROM v_pizz_id),
         ('2022-01-08'))
 ;
+REFRESH MATERIALIZED VIEW mv_dmitriy_visits_and_eats;
+SELECT * FROM mv_dmitriy_visits_and_eats;
