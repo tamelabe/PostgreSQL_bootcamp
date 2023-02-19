b12bc6a22d31bf1131549b3d975760864ae3e366
Tamela Bears <tamelabe@student.21-school.ru>
2023-02-19 19:58:15 +0300

    ex01 alpha

--- src/ex01/team01_ex01.sql
+++ src/ex01/team01_ex01.sql
@@ -1,15 +1,44 @@
-CREATE OR REPLACE FUNCTION check_date_in_the_past(curr_id integer, bal_date timestamp)
-       RETURNS TABLE (rate_to_usd numeric, date timestamp)
-   AS $$BEGIN
-        RETURN QUERY
-        (
-            SELECT rate_to_usd, c.updated
-            FROM currency c
-                JOIN balance b ON c.id = b.currency_id
+insert into currency values (100, 'EUR', 0.85, '2022-01-01 13:29');
+insert into currency values (100, 'EUR', 0.79, '2022-01-08 13:29');
 
-        );
-       END;
-    $$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION check_date(t_date timestamp, c_id bigint)
+    RETURNS numeric
+    LANGUAGE SQL AS
+    $$
+    (WITH id_fltred AS (
+        SELECT * FROM currency WHERE id = c_id),
+        d_past AS (
+            SELECT max(updated) AS date FROM id_fltred WHERE updated <= t_date),
+        d_future AS (
+            SELECT min(updated) AS date FROM id_fltred WHERE updated > t_date),
+        d_date AS (
+            SELECT (CASE
+                    WHEN (SELECT count(*) FROM d_past) = 1 THEN d_past.date
+                    ELSE d_future.date
+                    END) AS date
+            FROM d_past, d_future)
+    SELECT rate_to_usd
+    FROM currency c, d_date
+    WHERE d_date.date = c.updated)
+    $$;
+
+WITH currency_mod AS (
+        SELECT id, name, rate_to_usd, updated
+        FROM (
+            SELECT *, row_number() over (PARTITION BY id ORDER BY updated DESC) lxst
+            FROM currency c) AS que
+        WHERE lxst = 1)
+SELECT COALESCE(u.name, 'not defined') AS name,
+       COALESCE(u.lastname, 'not defined') AS lastname,
+       c.name AS currency_name,
+       (b.money::numeric * check_date(c.updated, c.id)) AS currency_in_usd
+FROM "user" AS u
+FULL JOIN balance b on u.id = b.user_id
+JOIN currency_mod c on c.id = b.currency_id
+ORDER BY name DESC, lastname, currency_name;
+
+
+
 
 
 SELECT DISTINCT COALESCE(u.name, 'not defined')   AS name,
