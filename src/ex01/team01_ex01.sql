62879841349baf562c06f5f3218bc7a555287163
Tamela Bears <tamelabe@student.21-school.ru>
2023-02-19 21:17:43 +0300

    ex01 alpha

--- src/ex01/team01_ex01.sql
+++ src/ex01/team01_ex01.sql
@@ -1,6 +1,8 @@
 insert into currency values (100, 'EUR', 0.85, '2022-01-01 13:29');
 insert into currency values (100, 'EUR', 0.79, '2022-01-08 13:29');
 
+DROP FUNCTION check_date(t_date timestamp, c_id bigint);
+
 CREATE OR REPLACE FUNCTION check_date(t_date timestamp, c_id bigint)
     RETURNS numeric
     LANGUAGE SQL AS
@@ -13,13 +15,13 @@ CREATE OR REPLACE FUNCTION check_date(t_date timestamp, c_id bigint)
             SELECT min(updated) AS date FROM id_fltred WHERE updated > t_date),
         d_date AS (
             SELECT (CASE
-                    WHEN (SELECT count(*) FROM d_past) = 1 THEN d_past.date
+                    WHEN (SELECT count(*) AS t FROM d_past WHERE d_past.date NOTNULL) = 1  THEN d_past.date
                     ELSE d_future.date
                     END) AS date
             FROM d_past, d_future)
     SELECT rate_to_usd
     FROM currency c, d_date
-    WHERE d_date.date = c.updated)
+    WHERE d_date.date = c.updated AND c.id = c_id)
     $$;
 
 WITH currency_mod AS (
@@ -31,35 +33,8 @@ WITH currency_mod AS (
 SELECT COALESCE(u.name, 'not defined') AS name,
        COALESCE(u.lastname, 'not defined') AS lastname,
        c.name AS currency_name,
-       (b.money::numeric * check_date(c.updated, c.id)) AS currency_in_usd
-FROM "user" AS u
-FULL JOIN balance b on u.id = b.user_id
+       (b.money::numeric * check_date(b.updated, c.id)) AS currency_in_usd
+FROM balance AS b
+FULL JOIN "user" u ON b.user_id = u.id
 JOIN currency_mod c on c.id = b.currency_id
 ORDER BY name DESC, lastname, currency_name;
-
-
-
-
-
-SELECT DISTINCT COALESCE(u.name, 'not defined')   AS name,
-                COALESCE(u.lastname, 'not defined') AS lastname,
-                user_id,
-                money,
-                currency_id,
-                b.updated
-FROM "user" u
-         JOIN balance b ON u.id = b.user_id
-WHERE currency_id IN (SELECT id FROM currency)
--- ORDER BY name DESC, lastname, currency_name;
-ORDER BY name DESC, lastname ASC, b.updated;
-
-
-
-
--- SELECT DISTINCT user_id, money, currency.rate_to_usd, balance.updated, currency.updated, (currency.updated - balance.updated) as dif
--- FROM balance
---     JOIN currency ON currency.id = balance.currency_id
--- WHERE currency_id = 100
--- ORDER BY dif
--- -- FETCH FIRST 1 ROWS ONLY
--- ;
